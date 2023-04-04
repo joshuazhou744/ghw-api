16980d21d2f57468b3d08cea27f1c51aa9315c74
Joshua <j.zhou11@share.epsb.ca>
2023-04-03 19:50:36 -0600

    Add new data

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
+
+hackathons = {
+    "Bitcamp": {
+        "start_date": "never"
+    }
+}
 
 @app.route("/")
 def hello_ghw():
     return "<p>Hello gang 404</p>"
 
-if __name__ == "__main__":
+@app.route('/getHackathons', methods=['GET'])
+def getHackathons():
+    return hackathons
+
+
+if __name__ =="__main__":
     app.run(debug = True)
